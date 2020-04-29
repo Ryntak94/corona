136444ca9a047044488558fea47a6e1179f77718
Ryan Matthews <ryan.matthews.professional@gmail.com>
2020-04-28 21:45:20 -0400

    add ability to sort

--- src/components/CardContainer.js
+++ src/components/CardContainer.js
@@ -10,6 +10,24 @@ const Container = styled.div`
     margin: 0 auto;
 `
 
+const SortBar = styled.div`
+    display: flex;
+    margin: 0 auto;
+    justify-content: center;
+`
+
+const SortButton = styled.div`
+    display: flex;
+    margin: 10px auto;
+    border: solid 1px black;
+    border-radius: 5px;
+    padding: 5px;
+    background: snow;
+    &:hover    {
+        cursor: pointer;
+    }
+`
+
 
 class CardContainer extends Component {
     constructor(props)  {
@@ -27,9 +45,52 @@ class CardContainer extends Component {
                 }))
             })
     }
+     onClickHandler = async ({target}) => {
+        console.log(target.getAttribute("name"))
+        if(target.getAttribute("name") === "aa") {
+            let sorted = await this.state.states.sort((a,b) => (a.state < b.state) ? -1 : 1)
+
+            this.setState((state) => ({
+                states: sorted
+            }), () => console.log(this.state.states))
+        }   else if(target.getAttribute("name") === "ad") {
+            let sorted = await this.state.states.sort((a,b) => (a.state < b.state) ? 1 : -1)
+
+            this.setState((state) => ({
+                states: sorted
+            }), () => console.log(this.state.states))
+        }   else if(target.getAttribute("name") === "ca") {
+            let sorted = await this.state.states.sort((a,b) => (a.cases < b.cases) ? -1 : 1)
+
+            this.setState((state) => ({
+                states: sorted
+            }), () => console.log(this.state.states))
+        }   else if(target.getAttribute("name") === "cd") {
+            let sorted = await this.state.states.sort((a,b) => (a.cases < b.cases) ? 1 : -1)
+
+            this.setState((state) => ({
+                states: sorted
+            }), () => console.log(this.state.states))
+        }
+    }
 
     render()    {
         return (
+            <>
+            <SortBar>
+            <SortButton name="aa" onClick={this.onClickHandler}>
+            Sort alphabetically, ascending
+            </SortButton>
+            <SortButton name="ad" onClick={this.onClickHandler}>
+            Sort alphabetically, descending
+            </SortButton>
+            <SortButton name="ca" onClick={this.onClickHandler}>
+            Sort by cases, ascending
+            </SortButton>
+            <SortButton name="cd" onClick={this.onClickHandler}>
+            Sort by cases, descending
+            </SortButton>
+            </SortBar>
           <Container>
             {
                 this.state.states.map((item) => {
@@ -38,6 +99,7 @@ class CardContainer extends Component {
                 })
             }
           </Container>
+          </>
         );
     }
 
